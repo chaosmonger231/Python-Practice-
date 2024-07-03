881f35155cc8e06947e78c5bcf39fc2240478ef4
chaosmonger231 <135679689+chaosmonger231@users.noreply.github.com>
2024-07-03 15:07:24 -0400

    Add files via upload

---
+++ pythonVersion.py
@@ -0,0 +1,6 @@
+#https://www.w3resource.com/python-exercises/python-basic-exercises.php
+#Problem 2 - Python Basic Exercises for Beginners
+#PROMPT: Write a Pything Program to find out what version of Python you are using
+
+import sys
+print(sys.version)
