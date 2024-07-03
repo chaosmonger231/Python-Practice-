138d910432a76a53c6611aaf67319c39ffe3e4c9
chaosmonger231 <135679689+chaosmonger231@users.noreply.github.com>
2024-07-03 14:55:51 -0400

    Add files via upload

---
+++ twinkleW3.py
@@ -0,0 +1,34 @@
+#twinklephrase
+
+def twinkle():
+  
+  indent_level = 8
+  indent = ' ' * indent_level
+  string = "Twinkle, twinkle, little star,"
+  string2 = "How I wonder what you are!"
+  
+  indented_string = f"{indent}{string2}"
+  
+  finalPrint = string + "\n" + indented_string
+  
+  print(finalPrint)
+  
+  
+def above():
+  
+  indent_level = 16
+  indent = ' ' * indent_level
+  string = "Up above the world so high,"
+  string2= "Like a diamond in the sky."
+  
+  indented_string = f"{indent}{string}"
+  indented_string2 = f"{indent}{string2}"
+  
+  
+  print(indented_string)
+  print(indented_string2)
+  
+twinkle()
+above()
+twinkle()
+  
